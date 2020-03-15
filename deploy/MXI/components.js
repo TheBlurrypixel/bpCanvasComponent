a7d59bdb7d1fca410ede26b39117a39f655d96aa
Michael Gochoco <mgochoco@yahoo.com>
2020-03-15 12:16:37 -0700

    Redeploy

--- deploy/MXI/components.js
+++ deploy/MXI/components.js
@@ -4,15 +4,15 @@
 		"className": "blurrypixel.Canvas",
 		"displayName": "DISP_NAME_IMAGE",
 		"version": "1.0",
-		"source": "src/blurrypixelCanvas.js",
-		"icon": "assets/SP_Image_Sm",
+		"source": "src/bpCanvasComponent.js",
+		"icon": "assets/bp_Image_Sm",
 		"dimensions": [400, 300],
 		"dependencies": [
 			{"src": "../lib/jquery-3.4.1.min.js", "cdn": "https://code.jquery.com/jquery-3.4.1.min.js"},
 			{"src": "../sdk/anwidget.js"}
 		],
 		"properties": [
-			{"name": "PROP_CLASS", "variable": "class", "type": "String", "default": "blurrypixelCanvas"},
+			{"name": "PROP_CLASS", "variable": "class", "type": "String", "default": "bpCanvas"},
 			{"name": "PROP_VISIBLE", "variable": "visible", "type": "Boolean", "default": "true"}
 		]
 	}]
